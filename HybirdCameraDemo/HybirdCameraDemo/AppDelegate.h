9ebd22d5c6b14e2bd894afc892a90d53d638ab66
Lision <lisionmail@gmail.com>
2017-10-17 21:49:20 +0800

    First Commit

---
+++ HybirdCameraDemo/HybirdCameraDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HybirdCameraDemo
+//
+//  Created by Lision on 2017/10/17.
+//  Copyright © 2017年 Lision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HybirdCameraDemo/HybirdCameraDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HybirdCameraDemo
+//
+//  Created by Lision on 2017/10/17.
+//  Copyright © 2017年 Lision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
